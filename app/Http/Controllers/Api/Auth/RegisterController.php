3e74c7f0f278a7d6eb59ca7e31252ce9dae8445b
Sultanbek Akilbaev <sulta.95@mail.ru>
2021-10-25 17:49:28 +0600

    Auth

---
+++ app/Http/Controllers/Api/Auth/RegisterController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\Auth\RegisterFormRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index(RegisterFormRequest $request)
+    {
+        $user = User::create(array_merge(
+            $request->only('name', 'email'),
+            ['password' => bcrypt($request->password)],
+        ));
+
+        return response()->json([
+            'message' => 'Вы успешно зарегистрировались.'
+        ], 200);
+    }
+}
