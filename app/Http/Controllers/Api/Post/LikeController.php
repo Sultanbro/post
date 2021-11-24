ce2426cfe7ed336b381db11754c7c0b15f1b313c
Sultanbek Akilbaev <sulta.95@mail.ru>
2021-11-24 15:49:41 +0600

    Post

--- app/Http/Controllers/Api/Post/LikeController.php
+++ app/Http/Controllers/Api/Post/LikeController.php
@@ -36,7 +36,7 @@ class LikeController extends Controller
     {
         if ($model = $this->likeReposytory->firstByPostId(array_merge($request->all(), ['user_id' => Auth::id()]))) {
             if ($this->likeReposytory->deleteById($model->id)) {
-                return ['like_count' => $this->likeReposytory->liked_count($model->parent_id, $model->type)];
+                return response()->json(['message' => 'ok'], 200);
             }
             return response()->json(['message' => 'Not Found'], 404);
         }
@@ -45,4 +45,17 @@ class LikeController extends Controller
         }
         return response()->json(['message' => 'not found'], 404);
     }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function update(LikeInfoRequest $request, $id)
+    {
+        return LikeResource::collection($this->likeReposytory->byParentIdType($id, $request->all()));
+    }
+
 }
