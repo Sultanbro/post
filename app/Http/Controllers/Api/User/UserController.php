2c352cab484938e7ab90bb509026d63451001b4c
Sultanbek Akilbaev <sulta.95@mail.ru>
2021-12-13 18:43:07 +0600

    User

--- app/Http/Controllers/Api/User/UserController.php
+++ app/Http/Controllers/Api/User/UserController.php
@@ -31,8 +31,27 @@ class UserController extends Controller
      */
     public function index()
     {
-        return new UserResource($this->userRepository->find(Auth::id()));
-//        if ()
+//        return new UserResource($this->userRepository->find(Auth::id()));
+        if (Auth::id() == 3) {
+            return ['mail' => 'master@mail.uz',
+                'position' => 'Ведущий Web-программист',
+                'education' => 'Казахский Национальный технический университет им. К.Сатпаева',
+                'city' => 'Нукус',
+                'b_day' => '30.03.1995',
+                'sick' => [0 => ['day' => '3', 'dates' => '14.10.2021-16.10.2021'],
+                            1 => ['day' => '7', 'period' => '15.11.2021-22.11.2021']],
+                'vacation' => [0 => ['type' => 'Ежегодный основной оплачиваемый отпуск',
+                                     'period' => '01.05.2021-31.04.2022',
+                                     'periodvac' => [0 => '11.09.2021-15.09.2021', 1 => '01.12.2021-05.12.2021'],
+                                     'duration' => '10', 'rest' => '18']],
+                'career' => [0 => ['datebeg' => '01.05.2021', 'dateend' => '01.08.2021',
+                                    'department' => 'Управление web-программирования', 'Web - программист'],
+                            1 => ['datebeg' => '01.08.2021', 'dateend' => '0',
+                                    'department' => 'Управление web-программирования', 'Ведущий Web-программист']]];
+        }
+        if (Auth::id() == 29) {
+            return ['mail' => 'test@mail.ru', 'position' => 'Web-программист', 'education' => 'МУИТ', 'city' => 'Караганда', 'b_day' => '22.10.1988'];
+        }
     }
 
     /**
