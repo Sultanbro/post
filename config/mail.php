311893c4f0990d3137eb5cda0354abc07e252429
sultanbro <sulta.95@mail.ru>
2022-01-21 14:46:22 +0600

    email

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'DJumagulov@cic.kz'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
         'mail_from' => env('MAIL_FROM', ''),
     ],
