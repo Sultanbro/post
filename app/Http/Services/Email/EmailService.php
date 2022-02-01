9d0e5bbb4885dfe2a92137158478db589d572020
Sultanbek Akilbaev <sulta.95@mail.ru>
2022-02-01 10:51:59 +0600

    Email

--- app/Http/Services/Email/EmailService.php
+++ app/Http/Services/Email/EmailService.php
@@ -74,7 +74,7 @@ class EmailService implements EmailServiceInterface
                     $details['user_id'] = $user->id;
                     $details['email_content']['url'] = "https://mycent.kz/auth/" . $this->userAuthService->tokenResetPassword($user) . "/$user->username";
                     $details['email_content']['username'] = $user->username;
-                    $details['email_content']['company_name'] = $user->company->fullname;
+                    $details['email_content']['company_name'] = $user->company->full_name;
                     $details['email'] = $user->email;
                     dispatch(new SendEmailJob($details));
                 }else {
@@ -84,7 +84,7 @@ class EmailService implements EmailServiceInterface
             if ($result) {
                 return $result;
             }else {
-                return 'ok';
+                return $users;
             }
         }else{
             return $result;
