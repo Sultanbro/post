7ebd0277852a7771e399d7948723a84b4fa2fe11
Sultanbek Akilbaev <sulta.95@mail.ru>
2022-02-11 09:22:12 +0600

    Avatar

--- app/Http/Resources/User/Avatar/AvatarResource.php
+++ app/Http/Resources/User/Avatar/AvatarResource.php
@@ -14,6 +14,10 @@ class AvatarResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'client_id' => $this->client_id,
+            'link' => $this->link,
+        ];
     }
 }
