105e7243a37b0e5d6933de7a8bef99fcd5c3993e
Sultanbek Akilbaev <sulta.95@mail.ru>
2022-02-11 14:38:30 +0600

    Role table add

--- database/migrations/2022_02_01_102328_create_roles_table.php
+++ database/migrations/2022_02_01_102328_create_roles_table.php
@@ -17,6 +17,7 @@ class CreateRolesTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('slug');
+            $table->foreignId('company_id')->constrained('departments')->cascadeOnDelete();
             $table->timestampsTz();
         });
     }
