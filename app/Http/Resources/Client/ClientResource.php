0f3f2f55549d8b5861147cb3bb3a5b324dfa24ac
Sultanbek Akilbaev <sulta.95@mail.ru>
2022-03-09 10:55:15 +0600

    Client tree

--- app/Http/Resources/Client/ClientResource.php
+++ app/Http/Resources/Client/ClientResource.php
@@ -16,7 +16,12 @@ class ClientResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'first_name' => isset($this->clientInfo->first_name) ? $this->clientInfo->first_name : null,
+            'last_name' => isset($this->clientInfo->last_name) ? $this->clientInfo->last_name : null,
+            'parent_name' => isset($this->clientInfo->parent_name) ? $this->clientInfo->parent_name : null,
+            'short_name' => isset($this->clientInfo->short_name) ? $this->clientInfo->short_name : null,
             'full_name' => isset($this->clientInfo->full_name) ? $this->clientInfo->full_name : null,
+            'birthday' => isset($this->clientInfo->birthday) ? $this->clientInfo->birthday : null,
             'avatar' => isset($this->clientInfo->avatar) ? $this->clientInfo->avatar : ['link' => 'storage/default/avatar/' . $this->clientInfo->sex . '/photo.jpg'],
         ];
     }
